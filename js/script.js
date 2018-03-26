f4c5316701b985ac8c2ca30a575a08b39b4f9e6e
Emilia Walczak <emilia.walczak.87@wp.pl>
2018-03-27 00:02:30 +0200

    menu functions

--- js/script.js
+++ js/script.js
@@ -6,17 +6,50 @@ var omnie = $("#omnie");
 var zakres = $("#zakres");
 var kontakt = $("#kontakt");
 
-menuOmnie.click(showOmnie);
+menuOmnie.click(showOmnie);  //menu on click in big version
   function showOmnie() {
     omnie.removeClass("hidden")
+    omnie.addClass("text")
+    $(".warsaw").addClass("hidden")
+    zakres.addClass("hidden")
+    zakres.removeClass("text")
+    kontakt.addClass("hidden")
+    kontakt.removeClass("text")
 }
 
-menuZakres.click(showZakres);
+menuZakres.click(showZakres); //menu on click in big version
 function showZakres(){
   zakres.removeClass("hidden")
+  zakres.addClass("text")
+  $(".warsaw").addClass("hidden")
+  omnie.addClass("hidden")
+  omnie.removeClass("text")
+  kontakt.addClass("hidden")
+  kontakt.removeClass("text")
 }
 
-menuKontakt.click(showKontakt);
+menuKontakt.click(showKontakt); //menu on click in big version
 function showKontakt(){
   kontakt.removeClass("hidden")
+  kontakt.addClass("text")
+  $(".warsaw").addClass("hidden")
+  omnie.addClass("hidden")
+  omnie.removeClass("text")
+  zakres.addClass("hidden")
+  zakres.removeClass("text")
+}
+
+//to click hamburger menu in small version
+let setMenu = $(".bar")
+$(".small").click(openBar)
+function openBar(){
+  setMenu.toggleClass("big")
+  setMenu.toggleClass("display")
+}
+
+//to reload page:
+
+$("h1").click(backToMain)
+function backToMain(){
+  window.location.reload()
 }
